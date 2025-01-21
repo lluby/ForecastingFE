534d0ce550145c0d65ffa8e2fd049e5e6c647eed
lluby <elianaluby5243@gmail.com>
2025-01-21 12:58:41 +0700

    b

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Ubah ke "build" jika ingin output di folder "build"
-  },
+    outDir: 'build', // Pastikan output folder di sini sesuai
+  }
 })
